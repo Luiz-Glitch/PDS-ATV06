da8032c13f4f8fb21b15191d7bfe7819c14b44ce
Luiz Bessa <lbessaregis@gmail.com>
2024-08-05 10:25:23 -0300

    Add professor and disciplina models

--- escola/models.py
+++ escola/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Professor(models.Model):
+    nome = models.CharField(max_length=100)
+    data_entrada = models.DateField(auto_now_add=True)
+    matricula = models.CharField(max_length=100)
+    endereco = models.CharField(max_length=200)
+    
+class Disciplina(models.Model):
+    nome = models.CharField(max_length=100)
+    carga_horaria = models.IntegerField()
+    professor = models.ForeignKey(Professor, on_delete=models.CASCADE)
