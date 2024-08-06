461e59e2d81f2ba409f0f44a13e00762780eb597
JoaoOtavio13 <otavio.pessoa@escolar.ifrn.edu.br>
2024-08-05 22:03:38 -0300

    criação dos Models

--- escola/models.py
+++ escola/models.py
@@ -1,3 +1,21 @@
+from typing import Any
 from django.db import models
 
 # Create your models here.
+class Escola(models.Model):
+    nome=models.CharField(max_length=200)
+    local=models.CharField(max_length=200)
+    nivel_de_ensino=models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.nome
+
+class Materia(models.Model):
+    nome=models.CharField(max_length=200)
+    hora_aula=models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.nome
+    
+# criar os models aluno e professor e receber Escola e Materia
+
